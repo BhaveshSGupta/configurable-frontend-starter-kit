40ac35de21208cdab37c3c358a8ef39f67e02b22
Ali Syahidin <ali.idinz@gmail.com>
2018-10-08 09:45:50 +0700

    :sparkles: create gulp config file

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync').create()
+
+const baseDir = './src';
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: baseDir
+    },
+  })
+})
+
+gulp.task('sass', function() {
+    return gulp.src(baseDir + '/stylesheets/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch(baseDir + '/stylesheets/*.scss', ['sass'])
+
+  gulp.watch(baseDir + '/javascripts/*.js', browserSync.reload)
+})
